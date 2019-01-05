e036072b935801112761630a251f1fd162ecc65b
Alfonso Rios <malforime@gmail.com>
2019-01-04 20:39:34 -0600

    FTW

--- models/Node.js
+++ models/Node.js
@@ -1,55 +1,58 @@
-class Node {
+(() => {
+  class Node {
     constructor(){
-        this._question = null;
-        this._options = new Map;
-        this._isFinal = false;
-        this._level = null;
-        this._repeats = 0;
-        this._from = null;
-        this._lambda = (input) =>{
-            this._repeats ++;
-            return this._options.get(input);
-            
-        }
+      this._question = null;
+      this._options = new Map;
+      this._isFinal = false;
+      this._level = null;
+      this._repeats = 0;
+      this._from = null;
+      this._lambda = (input) =>{
+        this._repeats ++;
+        return this._options.get(input);
+
+      }
     }
 
     get question(){
-        return this._question;
+      return this._question;
     }
 
     set question(data){
-        this._question = data;
+      this._question = data;
     }
 
     get options(){
-        return this._options
+      return this._options
     }
 
     set options(transactions){
-        this._options = transactions
+      this._options = transactions
     }
 
     get isFinal(){
-        return this._isFinal
+      return this._isFinal
     }
 
     set isFinal(value){
-        this._isFinal = value
+      this._isFinal = value
     }
 
     get level(){
-        return this._level
+      return this._level
     }
 
     set level(value){
-        this._level = value
+      this._level = value
     }
 
     get from(){
-        return this._from
+      return this._from
     }
 
     set from(value){
-        this._from = value
+      this._from = value
     }
-}
+  }
+  Polymer.Node = Node;
+})();
