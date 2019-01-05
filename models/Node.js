62128dc751320e6606df6d3f24b357032e022efe
jorgeme0996 <jorgeme0996@gmail.com>
2019-01-04 19:21:49 -0600

    Ultimo update

--- models/Node.js
+++ models/Node.js
@@ -1,11 +1,16 @@
-'use strict'
-
 class Node {
     constructor(){
         this._question = null;
-        this.transactions = new Map;
-        this.isFinal = false;
-        this.level = null;
+        this._options = new Map;
+        this._isFinal = false;
+        this._level = null;
+        this._repeats = 0;
+        this._from = null;
+        this._lambda = (input) =>{
+            this._repeats ++;
+            return this._options.get(input);
+            
+        }
     }
 
     get question(){
@@ -14,5 +19,37 @@ class Node {
 
     set question(data){
         this._question = data;
+    }
+
+    get options(){
+        return this._options
+    }
+
+    set options(transactions){
+        this._options = transactions
+    }
+
+    get isFinal(){
+        return this._isFinal
+    }
+
+    set isFinal(value){
+        this._isFinal = value
+    }
+
+    get level(){
+        return this._level
+    }
+
+    set level(value){
+        this._level = value
+    }
+
+    get from(){
+        return this._from
+    }
+
+    set from(value){
+        this._from = value
     }
 }
