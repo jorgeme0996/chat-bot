04b8afbe4ea59380b6e7ac6586141b2b6698e593
jorgeme0996 <jorgeme0996@gmail.com>
2019-01-08 21:40:06 -0600

    Ultima versión

--- models/Node.js
+++ models/Node.js
@@ -3,9 +3,6 @@
     constructor(){
       this._question = null;
       this._options = new Map;
-      this._isFinal = false;
-      this._level = null;
-      this._repeats = 0;
       this._from = null;
     }
 
@@ -23,22 +20,6 @@
 
     set options(transactions){
       this._options = transactions
-    }
-
-    get isFinal(){
-      return this._isFinal
-    }
-
-    set isFinal(value){
-      this._isFinal = value
-    }
-
-    get level(){
-      return this._level
-    }
-
-    set level(value){
-      this._level = value
     }
 
     get from(){
